6be2114458d9e84bcc79edef68987223f30c3dd7
foozzi <foozzione@gmail.com>
2024-01-07 12:49:12 -0500

    07012024

--- linux/.config/nvim/lua/plugins/comment.lua
+++ linux/.config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,17 @@
+local config = function()
+  require('ts_context_commentstring').setup {
+    enable_autocmd = false,
+  }
+  require('Comment').setup {
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  }
+end
+
 return {
 	"numToStr/Comment.nvim",
-	opts = {
-	},
+	opts = config,
 	lazy = false,
+  dependencies = {
+    {"JoosepAlviste/nvim-ts-context-commentstring"}
+  }
 }
