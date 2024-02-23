3a34489acc8b6f6b8894edb480979f22aebfc94d
foozzi <foozzione@gmail.com>
2024-02-23 15:15:15 -0500

    23022024-2

--- global/.config/nvim/lua/plugins/mkdnflow.lua
+++ global/.config/nvim/lua/plugins/mkdnflow.lua
@@ -1,23 +1,23 @@
 return {
-  'jakewvincent/mkdnflow.nvim',
-  lazy = false,
-  config = function()
-    require('mkdnflow').setup({
-      modules = {
-        bib = true,
-        buffers = true,
-        conceal = true,
-        cursor = true,
-        folds = true,
-        links = true,
-        lists = true,
-        maps = true,
-        paths = true,
-        tables = true,
-        yaml = true,
-        cmp = true
-      },
-      filetypes = {md = true, rmd = true, markdown = true, telekasten = true}
-    })
-  end
+	"foozzi/mkdnflow.nvim",
+	lazy = false,
+	config = function()
+		require("mkdnflow").setup({
+			modules = {
+				bib = true,
+				buffers = true,
+				conceal = true,
+				cursor = true,
+				folds = true,
+				links = true,
+				lists = true,
+				maps = true,
+				paths = true,
+				tables = true,
+				yaml = true,
+				cmp = true,
+			},
+			filetypes = { md = true, rmd = true, markdown = true, telekasten = true },
+		})
+	end,
 }
