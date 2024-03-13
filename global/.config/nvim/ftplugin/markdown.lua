414cc148b810a9751cdd0a3eeca3b41d3579eac6
foozzi <foozzione@gmail.com>
2024-03-13 14:31:00 -0400

    13032024

--- global/.config/nvim/ftplugin/markdown.lua
+++ global/.config/nvim/ftplugin/markdown.lua
@@ -1,3 +1,10 @@
-vim.api.nvim_set_keymap("n", "<leader>mdp", ":lua require('utils.helpers').InsertMarkdownURL()<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>mdp",
+	":lua require('utils.helpers').InsertMarkdownURL()<CR>",
+	{ silent = true, noremap = true }
+)
+vim.opt_local.textwidth = 79
+vim.opt_local.wrap = true
 
 -- vim.cmd([[set briopt+=list:-1]])
