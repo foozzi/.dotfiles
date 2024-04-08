cee0553fa5e9af77ff5a2ce16ddf3a86bd57dab0
foozzi <foozzione@gmail.com>
2024-04-07 21:06:47 -0400

    07042024

--- global/.config/nvim/ftplugin/markdown.lua
+++ global/.config/nvim/ftplugin/markdown.lua
@@ -1,9 +1,13 @@
+-- inserting a link with a title
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>mdp",
 	":lua require('utils.helpers').InsertMarkdownURL()<CR>",
 	{ silent = true, noremap = true }
 )
+-- formattig a markdown table
+vim.api.nvim_set_keymap("n", "<leader>ftt", ":TableFormat<CR>", { silent = true, noremap = true })
+
 vim.opt_local.textwidth = 79
 vim.opt_local.wrap = true
 
