865e9e891f34786a5c849894df618048c466bb03
foozzi <foozzione@gmail.com>
2024-06-16 19:25:38 +0200

    16062024-2

--- global/.config/nvim/lua/core/autocmds.lua
+++ global/.config/nvim/lua/core/autocmds.lua
@@ -4,7 +4,7 @@ local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group = lsp_fmt_group,
 	callback = function()
-		local efm = vim.lsp.get_active_clients({ name = "efm" })
+		local efm = vim.lsp.get_clients({ name = "efm" })
 
 		if vim.tbl_isempty(efm) then
 			return
@@ -23,7 +23,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = "*",
--- 	callback = helpers.UpdateOrAppendTimestamp,
--- })
+-- https://jrnl.sh/en/stable/privacy-and-security/#neovim
+vim.api.nvim_create_autocmd( {"BufNewFile","BufReadPre" }, {
+  group = vim.api.nvim_create_augroup("PrivateJrnl", {}),
+  pattern = "*.jrnl",
+  callback = function()
+    vim.o.shada = ""
+    vim.o.swapfile = false
+    vim.o.undofile = false
+    vim.o.backup = false
+    vim.o.writebackup = false
+    vim.o.shelltemp = false
+    vim.o.history = 0
+    vim.o.modeline = false
+    vim.o.secure = true
+  end,
+})
+
